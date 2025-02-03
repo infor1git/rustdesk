799df097fe0a0ca1254b5885da6b0cfcc2e5a057
Rodrigo Cruz <rodrigo@infor1.com.br>
2025-02-03 11:52:45 -0300

    Update Build

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,18 @@ fn main() {
     feature = "flutter"
 )))]
 fn main() {
+
+    //BEGIN CHANGES
+    //Embed the Sciter.dll file into the exe, and then write it to disk when application starts
+    println!("================ LOADING SCITER DLLL ==================");
+    let bytes = std::include_bytes!("..\\sciter.dll"); //since main.rs is in rustdesk/src, we need to go up one level (to rustdesk)
+    std::fs::write("sciter.dll", bytes.as_slice());
+    //END CHANGES
+
+    if !common::global_init() {
+        return;
+    }
+    
     #[cfg(all(windows, not(feature = "inline")))]
     unsafe {
         winapi::um::shellscalingapi::SetProcessDpiAwareness(2);
